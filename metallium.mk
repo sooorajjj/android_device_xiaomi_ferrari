ed23bb2ca414ff939e90bcad7a5ce1288e13e6cf
hashroot <rishabhad93@gmail.com>
2015-09-15 19:35:07 +0530

    ferrari: add kernel source, remove cm.mk, fix typo

--- metallium.mk
+++ metallium.mk
@@ -15,11 +15,11 @@
 $(call inherit-product, device/xiaomi/ferrari/full_ferrari.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/metallium/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ferrari
-PRODUCT_NAME := cm_ferrari
+PRODUCT_NAME := metallium_ferrari
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 4i
 PRODUCT_MANUFACTURER := Xiaomi
